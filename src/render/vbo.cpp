e3f11c7d20eb365196bb5ff212a9cec28c1e0cb4
Michael Bosse <metiscus@gmail.com>
2015-12-21 16:45:19 -0500

    Fixed indent

--- src/render/vbo.cpp
+++ src/render/vbo.cpp
@@ -53,26 +53,26 @@ int32_t VBO::_bindToGL(BindMode mode)
 {
     switch (mode)
     {
-    case ArrayBuffer:
-    {
-        return GL_ARRAY_BUFFER;
-        break;
-    }
-    case ElementArrayBuffer:
-    {
-        return GL_ELEMENT_ARRAY_BUFFER;
-        break;
-    }
-    case PixelPackBuffer:
-    {
-        return GL_PIXEL_PACK_BUFFER;
-        break;
-    }
-    case PixelUnpackBuffer:
-    {
-        return GL_PIXEL_UNPACK_BUFFER;
-        break;
-    }
+        case ArrayBuffer:
+        {
+            return GL_ARRAY_BUFFER;
+            break;
+        }
+        case ElementArrayBuffer:
+        {
+            return GL_ELEMENT_ARRAY_BUFFER;
+            break;
+        }
+        case PixelPackBuffer:
+        {
+            return GL_PIXEL_PACK_BUFFER;
+            break;
+        }
+        case PixelUnpackBuffer:
+        {
+            return GL_PIXEL_UNPACK_BUFFER;
+            break;
+        }
     }
 
     return 0;
@@ -83,51 +83,51 @@ int32_t VBO::_usageToGL(UsageType usage)
     int32_t usageEnum = 0;
     switch (usage)
     {
-    case StaticDraw:
-    {
-        usageEnum = GL_STATIC_DRAW;
-        break;
-    }
-    case StaticRead:
-    {
-        usageEnum = GL_STATIC_READ;
-        break;
-    }
-    case StaticCopy:
-    {
-        usageEnum = GL_STATIC_COPY;
-        break;
-    }
-    case DynamicDraw:
-    {
-        usageEnum = GL_DYNAMIC_DRAW;
-        break;
-    }
-    case DynamicRead:
-    {
-        usageEnum = GL_DYNAMIC_READ;
-        break;
-    }
-    case DynamicCopy:
-    {
-        usageEnum = GL_DYNAMIC_COPY;
-        break;
-    }
-    case StreamDraw:
-    {
-        usageEnum = GL_STREAM_DRAW;
-        break;
-    }
-    case StreamRead:
-    {
-        usageEnum = GL_STREAM_READ;
-        break;
-    }
-    case StreamCopy:
-    {
-        usageEnum = GL_STREAM_COPY;
-        break;
-    }
+        case StaticDraw:
+        {
+            usageEnum = GL_STATIC_DRAW;
+            break;
+        }
+        case StaticRead:
+        {
+            usageEnum = GL_STATIC_READ;
+            break;
+        }
+        case StaticCopy:
+        {
+            usageEnum = GL_STATIC_COPY;
+            break;
+        }
+        case DynamicDraw:
+        {
+            usageEnum = GL_DYNAMIC_DRAW;
+            break;
+        }
+        case DynamicRead:
+        {
+            usageEnum = GL_DYNAMIC_READ;
+            break;
+        }
+        case DynamicCopy:
+        {
+            usageEnum = GL_DYNAMIC_COPY;
+            break;
+        }
+        case StreamDraw:
+        {
+            usageEnum = GL_STREAM_DRAW;
+            break;
+        }
+        case StreamRead:
+        {
+            usageEnum = GL_STREAM_READ;
+            break;
+        }
+        case StreamCopy:
+        {
+            usageEnum = GL_STREAM_COPY;
+            break;
+        }
     }
 
     return usageEnum;
@@ -137,21 +137,21 @@ int32_t VBO::_mappingToGL(MappingType type)
 {
     switch (type)
     {
-    case ReadOnly:
-    {
-        return GL_READ_ONLY;
-        break;
-    }
-    case WriteOnly:
-    {
-        return GL_WRITE_ONLY;
-        break;
-    }
-    case ReadWrite:
-    {
-        return GL_READ_WRITE;
-        break;
-    }
+        case ReadOnly:
+        {
+            return GL_READ_ONLY;
+            break;
+        }
+        case WriteOnly:
+        {
+            return GL_WRITE_ONLY;
+            break;
+        }
+        case ReadWrite:
+        {
+            return GL_READ_WRITE;
+            break;
+        }
     }
 
     return 0;
